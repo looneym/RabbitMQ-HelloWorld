3f01f94c87eb23789f06c97a99b62d75ec34da92
Micheál Looney <hello@micheallooney.com>
2017-03-23 00:30:55 +0000

    Sender loops for a bit

--- src/main/java/rabbitTest/rabbitTest/Send.java
+++ src/main/java/rabbitTest/rabbitTest/Send.java
@@ -15,9 +15,13 @@ public class Send {
     Channel channel = connection.createChannel();
 
     channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-    String message = "Hello World!";
-    channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
-    System.out.println(" [x] Sent '" + message + "'");
+
+    for (int i = 0 ;i<=214748364 ; i++){
+        String message = Integer.toString(i);
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+        System.out.println(" [x] Sent '" + message + "'");
+    }
+
 
     channel.close();
     connection.close();
